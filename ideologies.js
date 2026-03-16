d8f918eea1d3e19339881af143f7363585c596e5
ObmijSelaw <81539176+ObmijSelaw@users.noreply.github.com>
2026-03-16 00:19:18 -0600

    Modify ideology stats and add new entries
    Updated various ideological stats to reflect new values and added new ideologies.

--- ideologies.js
+++ ideologies.js
@@ -3,17 +3,17 @@ ideologies = [
 
     {
         "name": "Marxism-Leninism",
-        "stats": { "econ": 90, "dipl": 65, "govt": 20, "scty": 55 }
+        "stats": { "econ": 90, "dipl": 60, "govt": 20, "scty": 50 }
     },
 
     {
         "name": "Stalinism",
-        "stats": { "econ": 100, "dipl": 30, "govt": 0, "scty": 45 }
+        "stats": { "econ": 100, "dipl": 30, "govt": 0, "scty": 30 }
     },
  
     {
         "name": "Trotskyism",
-        "stats": { "econ": 90, "dipl": 80, "govt": 40, "scty": 65 }
+        "stats": { "econ": 90, "dipl": 85, "govt": 40, "scty": 60 }
     },
 
     {
@@ -23,41 +23,45 @@ ideologies = [
     
     {
         "name": "Religious communism",
-        "stats": { "econ": 90, "dipl": 70, "govt": 50, "scty": 20 }
+        "stats": { "econ": 90, "dipl": 60, "govt": 20, "scty": 20 }
     },
     
     {
         "name": "Libertarian socialism",
-        "stats": { "econ": 85, "dipl": 75, "govt": 70, "scty": 65 }
+        "stats": { "econ": 85, "dipl": 75, "govt": 75, "scty": 70 }
     },
     
     {
         "name": "Democratic socialism",
-        "stats": { "econ": 85, "dipl": 70, "govt": 45, "scty": 65 }
+        "stats": { "econ": 85, "dipl": 65, "govt": 50, "scty": 70 }
     },
     
 // --- WELFARISTS ---
 
     {
         "name": "Bismarckism",
-        "stats": { "econ": 75, "dipl": 40, "govt": 40, "scty": 40 }
+        "stats": { "econ": 70, "dipl": 30, "govt": 25, "scty": 30 }
     },
 
     {
         "name": "Mutualism",
-        "stats": { "econ": 75, "dipl": 90, "govt": 90, "scty": 65 }
+        "stats": { "econ": 65, "dipl": 90, "govt": 100, "scty": 50 }
     },
     
     {
         "name": "Intersectional progressivism",
-        "stats": { "econ": 75, "dipl": 75, "govt": 45, "scty": 90 }
+        "stats": { "econ": 75, "dipl": 70, "govt": 35, "scty": 80 }
     },
     
     {
         "name": "Distributism",
-        "stats": { "econ": 75, "dipl": 60, "govt": 65, "scty": 20 }
+        "stats": { "econ": 80, "dipl": 40, "govt": 60, "scty": 20 }
     },
 
+    {
+        "name": "Social democracy",
+        "stats": { "econ": 75, "dipl": 65, "govt": 60, "scty": 70 }
+    },
  
 // --- CENTRISM ---
     {
@@ -69,51 +73,51 @@ ideologies = [
 
     {
         "name": "Classic liberalism",
-        "stats": { "econ": 25, "dipl": 60, "govt": 70, "scty": 60 }
+        "stats": { "econ": 25, "dipl": 50, "govt": 75, "scty": 50 }
     },
     
     {
         "name": "Social liberalism",
-        "stats": { "econ": 60, "dipl": 65, "govt": 70, "scty": 65 }
+        "stats": { "econ": 60, "dipl": 65, "govt": 75, "scty": 70 }
     },
     
     {
         "name": "Neoliberalism",
-        "stats": { "econ": 25, "dipl": 75, "govt": 70, "scty": 75 }
+        "stats": { "econ": 30, "dipl": 70, "govt": 50, "scty": 70 }
     },
     
 // --- CONSERVATISMS ---
 
     {
         "name": "Liberal conservatism",
-        "stats": { "econ": 25, "dipl": 40, "govt": 60, "scty": 40 }
+        "stats": { "econ": 25, "dipl": 50, "govt": 65, "scty": 30 }
     },
     
     {
         "name": "Paleoconservatism",
-        "stats": { "econ": 50, "dipl": 30, "govt": 50, "scty": 15 }
+        "stats": { "econ": 60, "dipl": 40, "govt": 40, "scty": 15 }
     },
     
     {
         "name": "Neoconservatism",
-        "stats": { "econ": 30, "dipl": 15, "govt": 40, "scty": 40 }
+        "stats": { "econ": 25, "dipl": 15, "govt": 50, "scty": 30 }
     },
     
     {
-        "name": "Christian democracy",
-        "stats": { "econ": 60, "dipl": 40, "govt": 50, "scty": 35 }
+        "name": "Communitarian conservatism",
+        "stats": { "econ": 60, "dipl": 65, "govt": 50, "scty": 40 }
     },
     
 // --- CAPITALISMS ---
 
     {
         "name": "Minarchism",
-        "stats": { "econ": 5, "dipl": 75, "govt": 80, "scty": 60 }
+        "stats": { "econ": 5, "dipl": 55, "govt": 80, "scty": 50 }
     },
     
     {
         "name": "Objectivism",
-        "stats": { "econ": 5, "dipl": 15, "govt": 80, "scty": 45 }
+        "stats": { "econ": 5, "dipl": 20, "govt": 80, "scty": 35 }
     },
     
     {
@@ -127,26 +131,47 @@ ideologies = [
     },
     
 // -- NATIONALIST AUTOCRACIES --
-
-    {
-        "name": "East Asian technocracy",
-        "stats": { "econ": 30, "dipl": 40, "govt": 25, "scty": 40 }
-    },
     
     {
         "name": "Fascism",
-        "stats": { "econ": 50, "dipl": 5, "govt": 5, "scty": 10 }
+        "stats": { "econ": 50, "dipl": 10, "govt": 10, "scty": 15 }
+    },
+
+    {
+        "name": "Nazism",
+        "stats": { "econ": 65, "dipl": 0, "govt": 0, "scty": 8 }
     },
     
     {
         "name": "National Bolshevism",
         "stats": { "econ": 85, "dipl": 5, "govt": 5, "scty": 15 }
     },
+
+   {
+        "name": "Revolutionary nationalism",
+        "stats": { "econ": 75, "dipl": 40, "govt": 20, "scty": 50 }
+    },
     
 // -- PRIMITIVISM --
     {
         "name": "Anarcho-primitivism",
-        "stats": { "econ": 70, "dipl": 90, "govt": 90, "scty": 5 }
+        "stats": { "econ": 70, "dipl": 50, "govt": 90, "scty": 5 }
+    },
+
+// -- TECHNOS --
+    {
+        "name": "Techno-optimism (effective accelerationism)",
+        "stats": { "econ": 5, "dipl": 50, "govt": 75, "scty": 75 }
+    },
+
+    {
+        "name": "American technocracy",
+        "stats": { "econ": 90, "dipl": 90, "govt": 15, "scty": 80 }
+    },
+
+    {
+        "name": "East Asian technocracy",
+        "stats": { "econ": 30, "dipl": 50, "govt": 30, "scty": 50 }
     },
     
 ];
